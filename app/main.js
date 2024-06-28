3567d698b18d40fb29153cfd57ace2adc58a649e
anujda09 <anujdamani9@gmail.com>
2024-06-28 16:46:26 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,21 +1,20 @@
 const fs = require("fs");
 const path = require("path");
-const zlib = require("zlib");
 
-const command = process.argv[2];
+const zlib = require("zlib");
+const crypto = require("crypto");
 const param = process.argv[3];
 
+const command = process.argv[2];
 switch (command) {
     case "init":
         createGitDirectory();
         break;
     case "cat-file":
-        const hash = process.argv[4];
-        if (param === "-p") readObject(hash);
+        catFile();
         break;
     case "hash-object":
-        const file = process.argv[4];
-        if (param === "-w") hashObject(file);
+        hashObject();
         break;
     case "ls-tree":
         lsTree();
@@ -25,37 +24,65 @@ switch (command) {
 }
 
 function createGitDirectory() {
-    console.log(process.cwd());
-    fs.mkdirSync(path.join(process.cwd(), ".git"), { recursive: true });
-    fs.mkdirSync(path.join(process.cwd(), ".git", "objects"), { recursive: true });
-    fs.mkdirSync(path.join(process.cwd(), ".git", "refs"), { recursive: true });
-    fs.writeFileSync(path.join(process.cwd(), ".git", "HEAD"), "ref: refs/heads/main\n");
+    // ./your_git.sh init
+    fs.mkdirSync(path.join(__dirname, ".git"), { recursive: true });
+    fs.mkdirSync(path.join(__dirname, ".git", "objects"), { recursive: true });
+    fs.mkdirSync(path.join(__dirname, ".git", "refs"), { recursive: true });
+
+    fs.writeFileSync(
+        path.join(__dirname, ".git", "HEAD"),
+        "ref: refs/heads/main\n"
+    );
     console.log("Initialized git directory");
 }
 
-function readObject(hash) {
-    const file = fs.readFileSync(path.join(process.cwd(), ".git", "objects", hash.slice(0, 2), hash.slice(2)));
-    const inflated = zlib.inflateSync(file);
-    let content = inflated.toString();
-
-    content = content.slice(content.indexOf("\0") + 1).replace(/\n/g, "");
+function catFile() {
+    // ./your-git.sh cat-file -p <hash>
+    const type = process.argv[3];
+    const hash = process.argv[4];
+    if (!type || !hash) {
+        throw new Error("Missing arguments");
+    }
+    if (type !== "-p") {
+        throw new Error(`Unknown flag ${type}`);
+    }
+    const file = fs.readFileSync(
+        path.join(__dirname, ".git", "objects", hash.slice(0, 2), hash.slice(2))
+    );
+    const decompressed = zlib.inflateSync(file);
+    const contentStart = decompressed.indexOf("\x00") + 1;
+    const content = decompressed.toString("utf-8", contentStart);
     process.stdout.write(content);
 }
 
-function hashObject(file) {
-    const fileContent = fs.readFileSync(path.join(process.cwd(), file));
-    const header = `blob ${fileContent.length}\0`;
-    const content = Buffer.concat([Buffer.from(header), fileContent]);
-    const hash = crypto.createHash("sha1").update(content).digest("hex");
-    const dir = path.join(process.cwd(), ".git", "objects", hash.slice(0, 2));
-    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
-    const compressed = zlib.deflateSync(content);
-    fs.writeFileSync(path.join(dir, hash.slice(2)), compressed);
+function hashObject() {
+    // .your-git.sh hash-object -w <file>
+    const flag = process.argv[3];
+    const file = process.argv[4];
+    if (!flag || !file) {
+        throw new Error("Missing arguments");
+    }
+    if (flag !== "-w") {
+        throw new Error(`Unknown flag ${flag}`);
+    }
+
+    const hash = hashFile(file);
     process.stdout.write(hash);
 }
 
-function lsTree() {
+function hashFile(file) {
+    const content = fs.readFileSync(file);
+    const header = `blob ${content.length}\x00`;
+    const store = zlib.deflateSync(Buffer.from(header + content));
+    const hash = crypto.createHash("sha1").update(store).digest("hex");
+    const hashPath = path.join(__dirname, ".git", "objects", hash.slice(0, 2));
+    fs.mkdirSync(hashPath, { recursive: true });
+    fs.writeFileSync(path.join(hashPath, hash.slice(2)), store);
+    return hash;
+}
 
+function lsTree() {
+    // ./your-git.sh ls-tree [--name-only] <hash>
     const isNameOnly = process.argv[3] === "--name-only";
     const hash = process.argv[isNameOnly ? 4 : 3];
     if (!hash) {
