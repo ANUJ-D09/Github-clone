f7c6fe241a3f323fe7e9a046eaa75c81fc3caa24
anujda09 <anujdamani9@gmail.com>
2024-07-05 15:19:44 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -36,18 +36,33 @@ switch (command) {
         process.exit(1);
 }
 
-function createTree() {
-    const flag = process.argv[3];
-    if (flag == "--name-only") {
-        const sha = process.argv[4];
-        const directory = sha.slice(0, 2);
-        const fileName = sha.slice(2);
-        const filePath = path.join(__dirname, ".git", "objects", directory, fileName);
-        let inflatedContent = zlib.inflateSync(fs.readFileSync(filePath)).toString().split('\0');
-        let content = inflatedContent.slice(1).filter(value => value.includes(" "));
-        let names = content.map(value => value.split(" ")[1]);
-        names.forEach((name) => process.stdout.write(`${name}\n`));
+function lsTree() {
+    const isNameOnly = process.argv[3];
+    let hash = '';
+    if (isNameOnly === '--name-only') {
+        //display the name only
+        hash = process.argv[4];
+    } else {
+        hash = process.argv[3];
     }
+    const dirName = hash.slice(0, 2);
+    const fileName = hash.slice(2);
+    const objectPath = path.join(__dirname, '.git', 'objects', dirName, fileName);
+    const dataFromFile = fs.readFileSync(objectPath);
+    //decrypt the data from the file
+    const inflated = zlib.inflateSync(dataFromFile);
+    //notice before encrypting the data what we do was we encrypt
+    //blob length/x00 so to get the previous string back what we need to do is split with /xoo
+    const enteries = inflated.toString('utf-8').split('\x00');
+    //enteries will be [blob length/x00, actual_file_content]
+    const dataFromTree = enteries.slice(1);
+    const names = dataFromTree
+        .filter((line) => line.includes(' '))
+        .map((line) => line.split(' ')[1]);
+    const namesString = names.join('\n');
+    const response = namesString.concat('\n');
+    //this is the regex pattern that tells to replace multiple global \n with single \n
+    process.stdout.write(response.replace(/\n\n/g, '\n'));
 }
 
 function createGitDirectory() {
