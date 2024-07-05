8cb97f3dc0b0f7bc4300a585a1f9649046272755
anujda09 <anujdamani9@gmail.com>
2024-07-05 20:59:49 +0530

    pass stage

--- app/modules/writeFolderAsTree.js
+++ app/modules/writeFolderAsTree.js
@@ -2,52 +2,48 @@ const fs = require("fs");
 const path = require("path");
 const { sha1 } = require("./utils");
 const zlib = require("zlib");
-const writeBlob = require("./writeBlob");
+const writeBlobObject = require("./writeBlob");
 
-function writeTree2(root) {
-    const filesAndDirs = fs
-        .readdirSync(root)
-        .filter((f) => f !== ".git" && f !== "main.js");
+function writeGitTree(rootDir) {
+    const entries = fs
+        .readdirSync(rootDir)
+        .filter((entry) => entry !== ".git" && entry !== "main.js")
+        .map((entry) => {
+            const fullPath = path.join(rootDir, entry);
+            if (fs.statSync(fullPath).isFile()) {
+                return {
+                    mode: 100644,
+                    name: entry,
+                    hash: writeBlobObject(true, fullPath),
+                };
+            } else {
+                return {
+                    mode: 40000,
+                    name: entry,
+                    hash: writeGitTree(fullPath),
+                };
+            }
+        });
 
-    const entries = [];
-    for (const file of filesAndDirs) {
-        const fullPath = path.join(root, file);
-        if (fs.statSync(fullPath).isFile()) {
-            entries.push({
-                mode: 100644,
-                name: file,
-                hash: writeBlob(true, fullPath),
-            });
-        } else {
-            entries.push({
-                mode: 40000,
-                name: file,
-                hash: writeTree2(path.join(root, file)),
-            });
-        }
-    }
-
-    const contents = entries.reduce((acc, { mode, name, hash }) => {
+    const treeContentsBuffer = entries.reduce((buffer, { mode, name, hash }) => {
         return Buffer.concat([
-            acc,
-            Buffer.from($ { mode } { name }\
-                0),
+            buffer,
+            Buffer.from(`${mode} ${name}\0`),
             Buffer.from(hash, "hex"),
         ]);
     }, Buffer.alloc(0));
-    const treeContents = Buffer.concat([
-        Buffer.from(tree $ { contents.length }\
-            x00),
-        contents,
-    ]);
-    const treeHash = sha1(treeContents);
-    fs.mkdirSync(path.join(root, ".git", "objects", treeHash.slice(0, 2)), {
-        recursive: true,
-    });
-    fs.writeFileSync(
-        path.join(root, ".git", "objects", treeHash.slice(0, 2), treeHash.slice(2)),
-        zlib.deflateSync(treeContents),
-    );
+
+    const treeHeader = Buffer.from(`tree ${treeContentsBuffer.length}\x00`);
+    const fullTreeBuffer = Buffer.concat([treeHeader, treeContentsBuffer]);
+    const treeHash = sha1(fullTreeBuffer);
+
+    const objectDir = path.join(rootDir, ".git", "objects", treeHash.slice(0, 2));
+    const objectPath = path.join(objectDir, treeHash.slice(2));
+
+    fs.mkdirSync(objectDir, { recursive: true });
+    fs.writeFileSync(objectPath, zlib.deflateSync(fullTreeBuffer));
+
     return treeHash;
 }
-module.exports = writeTree2;
+
+module.exports = writeGitTree;
